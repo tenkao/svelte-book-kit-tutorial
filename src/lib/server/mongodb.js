1698ea6eb7dd65b1cc48489a951f0b01772e45f0
Hagiwara Tsutomu <tenkao@gmail.com>
2023-06-09 17:18:14 +0900

    7.2.3 mongodb パッケージの導入と接続情報の管理
    MongoDB サーバへ接続するためのコードを追加

---
+++ src/lib/server/mongodb.js
@@ -0,0 +1,7 @@
+// MongoDB サーバへの接続
+
+import { MongoClient } from 'mongodb'
+import { env } from '$env/dynamic/private'
+
+export const client = new MongoClient(env.MONGODB_URI ?? 'mongodb://dummy')
+export const database = client.db()
