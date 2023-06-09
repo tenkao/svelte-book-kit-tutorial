3cba4df4a83a2bab4c436a510bb94d2274961244
Hagiwara Tsutomu <tenkao@gmail.com>
2023-06-09 18:55:15 +0900

    7.2.4 カートを MongoDB で実装
    カート内のデータの読み込み・保存先を JSON ファイルから MongoDB に変更

--- src/lib/server/cart.js
+++ src/lib/server/cart.js
@@ -1,28 +1,14 @@
 // カートの処理を行うサーバーサイドのコード
 
-import { readFile, writeFile } from 'fs/promises'
-
-const dataPath = 'data/cart.json'
+import { database } from '$lib/server/mongodb'
 
 // カートに商品データを追加する
 export const addToCart = async (productId) => {
-  const cart = await loadCart()
-  if (!cart.includes(productId)) {
-    cart.push(productId)
-  }
-  await writeFile(dataPath, JSON.stringify(cart), { encoding: 'utf-8' })
+  await database.collection('cart').insertOne({ productId })
 }
 
 // カート内のデータを取得する
 export const loadCart = async () => {
-  try {
-    const content = await readFile(dataPath)
-    return JSON.parse(content)
-  } catch (err) {
-    if (err.code === 'ENOENT') {
-      return []
-    } else {
-      throw err
-    }
-  }
+  const cart = await database.collection('cart').find()
+  return await cart.map((doc) => doc.productId).toArray()
 }
