cc93b83a7ceaa04631805725cef76cd692128562
Hagiwara Tsutomu <tenkao@gmail.com>
2023-06-14 17:16:02 +0900

    8.2.5 セッション情報をデータベースに保存
    するための処理を追加

---
+++ src/lib/server/session.js
@@ -0,0 +1,18 @@
+// セッションの作成・保存を行うためのライブラリ
+
+import { database } from '$lib/server/mongodb'
+
+// セッションの有効期限
+const expiresIn = 30 * 60 * 1000 // 30分
+
+// セッションを作成する
+const createSession = async (data) => {
+  const sessionId = crypto.randomUUID()
+  const session = {
+    _id: sessionId,
+    expiresAt: Date.now() + expiresIn,
+    ...data,
+  }
+  await database.collection('sessions').insertOne(session)
+  return sessionId
+}
