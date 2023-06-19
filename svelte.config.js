ba6d7c0cb603b7051fccdbe80ec076d40f5347e4
Hagiwara Tsutomu <tenkao@gmail.com>
2023-06-19 18:58:15 +0900

    9.2.5 プリレンダリングの動作確認
    build 用の設定を追加

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
-};
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter(),
+    prerender: {
+      entries: ['/products/svelte-book', '/products/react-book', '/products/vue-book', '/products/angular-book'],
+      origin: 'https://svelte-book-kit-tutorial-psi.vercel.app',
+    },
+  },
+}
 
-export default config;
+export default config
